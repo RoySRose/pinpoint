c2129fc68e1fde42eacd1768c3cc7980201778c9
Roy Kim <sungwook0115.kim@gmail.com>
2018-07-12 18:22:53 +0900

    ㅎ

--- scripts/deployGitPage.sh
+++ scripts/deployGitPage.sh
@@ -15,7 +15,7 @@ git config --global user.email "sungwook0115.kim@gmail.com"
 git config --global user.name "RoySRose"
 
 #using token clone gh-pages branch
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://ZBrxvfeGzS5jiUsUJ16QBu1PdO5WOf1seGQXdDPzOQBtR9KhoJoRX0PNq+wiSsg/9quJXGQ4+zTLUnpZB+v3jgcnSG8Dhvc+Vu1soNHjFcs+sgmL18nnKOe/sPjucBA5ViNxRqYH1mVyIwwqYJXdy8I6BbsbgM7JT6JspD4Q8qr7deHQ2QQCB7h1OqnzjBfwpTxYNw2sbbYVCBpSsgUxGJkOJRqJU84pAJ+MMSke3lVG46ptlJlfEP5930Z+n4oEJGIx0Yo7L1AIovJyerneQqxgSIAYm7A8os6wU38BcgxA5BP5qGins0cZ6TCSETBnyuQbgP3Lm6Kb2Uudezi01/aegd+x4Hl2n0S37pgKk32a3WLqLJMwZWEtGRiF6HNdwxBNuNsA1qYG5aF2utiCPsgVcwJJrMKXa/lZuvwMmWKogc6GNrhOCA8B8ck1VafYhbhJkWaERYXNMapvMbUWqnklygE/dbQxpgzbxwm9UWuFuqBhbn6P5jB6vracbXHmotTvtnB1kbmzH7FfqQ6WQi1nRR3GvAq0U7l+26FozprjsdmDAWV6I8/LTpoI9LsjjKV+E6sm+CHqKnIh/594H/T/gkbBpggpdqgO6i2yfrsHuvjsybFWFX6NZ71+w45LD0oP5adQSPBXGICOcIID83Bor3Vsr47rE7zL6BH0t0M=@github.com/RoySRose/pinpoint.git gh-pages > /dev/null
 
 #go into directory and copy data we're interested in to that directory
 cd gh-pages
